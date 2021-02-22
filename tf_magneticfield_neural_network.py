85e23254874ab9b31bef862bb7fba6fbd2ab3438
jean-francois.viardot <jean-francois.viardot@insa-cvl.fr>
2021-02-22 11:10:12 +0100

    CodePython

---
+++ tf_magneticfield_neural_network.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+#abalone_train = pd.read_csv("data.csv",names=["x", "y", "z", "r1", "r2",
+           "t1", "t2", "V"])
+
+
+abalone_train.head()
+
+abalone_features=abalone_train.copy()
+abalone_labels = abalone_features.pop('V')
+
+abalone_features=np.array(abalone_features)
+abalone_features
+
+abalone_model = tf.keras.Sequential([
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+abalone_model.compile(loss = tf.losses.MeanSquaredError(),
+                      optimizer = tf.optimizers.Adam())
+					  
+
+abalone_model.fit(abalone_features, abalone_labels, epochs=100)
+
+normalize = preprocessing.Normalization()
+normalize.adapt(abalone_features)
+
+norm_abalone_model = tf.keras.Sequential([
+  normalize,
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+norm_abalone_model.compile(loss = tf.losses.MeanSquaredError(),
+                           optimizer = tf.optimizers.Adam())
+
+norm_abalone_model.fit(abalone_features, abalone_labels, epochs=100)
